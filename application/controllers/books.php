ecc54d5aad62146296a5cc3e6c96c4fb2136646b
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-11-24 13:26:05 +0600

    satup in my ubuntu 14.04 and fix .htacces in my etc/apache2/apache.conf var/www/ to replace None with All

--- application/controllers/books.php
+++ application/controllers/books.php
@@ -1,30 +1,30 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class Books extends CI_Controller 
-{
-	 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-	public $uid;
+class Books extends CI_Controller {
+
+    public $uid;
     public $module;
 
     public function __construct() {
-    parent::__construct();
+        parent::__construct();
 
-    $this->load->model('Commons', 'CM') ;  
-    $this->module='user';
-    $this->uid=$this->session->userdata('uid');
+        $this->load->model('Commons', 'CM');
+        $this->module = 'user';
+        $this->uid = $this->session->userdata('uid');
     }
 
-    public function index()
-    {
-    	$data['books_list']=$this->CM->getTotalALL('books');
-       
-        
-    	$no_rows= $this->CM->getTotalRow('books');
+    public function index() {
+        $data['books_list'] = $this->CM->getTotalALL('books');
+
+
+        $no_rows = $this->CM->getTotalRow('books');
         $this->load->library('pagination');
-        $config['base_url'] = base_url().'books/index/';
-       
-        $config['total_rows'] = $no_rows ;
+        $config['base_url'] = base_url() . 'books/index/';
+
+        $config['total_rows'] = $no_rows;
         $config['per_page'] = 15;
         $config['full_tag_open'] = '<div class=" text-center"><ul class=" list-inline list-unstyled " id="listpagiction">';
         $config['full_tag_close'] = '</ul></div>';
@@ -40,22 +40,21 @@ class Books extends CI_Controller
         $config['num_tag_close'] = '</li>';
         $config['last_link'] = 'Last';
         $config['first_link'] = 'First';
-        $this->pagination->initialize($config);     
-        
-        $data['books_list']=$this->CM->getTotalALL('books',$this->uri->segment(3), $config['per_page']);
-        $this->load->view('books/index',$data);    
+        $this->pagination->initialize($config);
+
+        $data['books_list'] = $this->CM->getTotalALL('books', $this->uri->segment(3), $config['per_page']);
+        $this->load->view('books/index', $data);
     }
 
-    public function add()
-    {
-      if( !$this->CM->checkpermission($this->module,'add', $this->uid))
-             redirect ('error/accessdeny');
-      
-       
+    public function add() {
+        if (!$this->CM->checkpermission($this->module, 'add', $this->uid))
+            redirect('error/accessdeny');
 
-        $data['group_list']=$this->CM->getAll('group', 'name ASC' );
-        $data['subject_list']=$this->CM->getAll('tbl_subject', 'name ASC' );
-        
+
+
+        $data['group_list'] = $this->CM->getAll('group', 'name ASC');
+        $data['subject_list'] = $this->CM->getAll('tbl_subject', 'name ASC');
+
 //        echo '<pre>';
 //        print_r($data['subject_list']);
 //        exit();
@@ -65,73 +64,62 @@ class Books extends CI_Controller
         $data['book_code'] = "";
         $data['subject_name'] = "";
         $data['book_rate'] = "";
-        
-      
+
+
         $this->load->library('form_validation');
 
 
         $this->form_validation->set_rules('name', 'required', 'address');
         $this->form_validation->set_rules('subject_name', 'required', 'address');
-        if ($this->form_validation->run() == FALSE)
-        {
-            $this->load->view('books/form', $data); 
-        }
-        else
-        {
-            
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('books/form', $data);
+        } else {
+
             $datas['book_name'] = $this->input->post('book_name');
             $datas['group_id'] = $this->input->post('group_id');
             $datas['book_code'] = $this->input->post('book_code');
             $datas['subject_id'] = $this->input->post('subject_id');
             $datas['rate'] = $this->input->post('rate');
-            
+
 
             $datas['status'] = 1;
             //$datas['entryby']=$this->session->userdata('uid');       
-            
 
-            $insert = $this->CM->insert('books',$datas) ; 
-            if($insert)
-            {
+
+            $insert = $this->CM->insert('books', $datas);
+            if ($insert) {
                 $msg = "Operation Successfull!!";
-        		$this->session->set_flashdata('success', $msg);
-                redirect('books'); 
-            }
-            else 
-            {
+                $this->session->set_flashdata('success', $msg);
+                redirect('books');
+            } else {
                 $msg = "There is an error, Please try again!!";
-        		$this->session->set_flashdata('error', $msg);
-        		$this->load->view('college/form', $data); 
+                $this->session->set_flashdata('error', $msg);
+                $this->load->view('college/form', $data);
             }
-              redirect('books','refresh'); 
+            redirect('books', 'refresh');
         }
-        
     }
 
-    public function edit($id)
-    {
-         if( !$this->CM->checkpermission($this->module,'edit', $this->uid))
-             redirect ('error/accessdeny');
-        
-        $content = $this->CM->getInfo('books', $id) ; 
-        $data['group_list']=$this->CM->getAll('group', 'name ASC' );
-        $data['subject_list']=$this->CM->getAll('tbl_subject', 'name ASC' );
-        
+    public function edit($id) {
+        if (!$this->CM->checkpermission($this->module, 'edit', $this->uid))
+            redirect('error/accessdeny');
+
+        $content = $this->CM->getInfo('books', $id);
+        $data['group_list'] = $this->CM->getAll('group', 'name ASC');
+        $data['subject_list'] = $this->CM->getAll('tbl_subject', 'name ASC');
+
         $data['name'] = $content->book_name;
         $data['group_id'] = $content->group_id;
         $data['book_code'] = $content->book_code;
         $datas['subject_id'] = $this->input->post('subject_id');
         $data['book_rate'] = $content->rate;
         //$data['status'] = $content->status;
-        
+
         $this->load->library('form_validation');
-        $this->form_validation->set_rules( 'name', 'required', 'address');
-        if ($this->form_validation->run() == FALSE)
-        {
-                $this->load->view('books/form', $data); 
-        }
-        else
-        {
+        $this->form_validation->set_rules('name', 'required', 'address');
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('books/form', $data);
+        } else {
             $datas['book_name'] = $this->input->post('book_name');
             $datas['group_id'] = $this->input->post('group_id');
             $datas['book_code'] = $this->input->post('book_code');
@@ -140,12 +128,12 @@ class Books extends CI_Controller
             //$datas['status'] = $this->input->post('status');
             //$datas['entryby']=$this->session->userdata('uid');       
 
-                if($this->CM->update('books', $datas, $id)){
-                    $msg = "Operation Successfull!!";
-                    $this->session->set_flashdata('success', $msg);
-                    redirect('books'); 
-                }
+            if ($this->CM->update('books', $datas, $id)) {
+                $msg = "Operation Successfull!!";
+                $this->session->set_flashdata('success', $msg);
+                redirect('books');
+            }
         }
-        
     }
+
 }
